871dfd9b32816282acdcfee279c9dbb92dc9b73c
dawnhunter <jmariemayer@outlook.com>
2015-01-18 21:23:04 -0600

    adding cookie info

--- template.php
+++ template.php
@@ -1,4 +1,14 @@
 <?php 
+if (isset($_GET['choice'])) { // If the choice variable is set in the URL variable (if they choose a style)
+	$choice = $_GET['choice']; // Put choice URL variable value into a local PHP variable $choice
+	if ($choice == "Dark" || $choice == "Light") { // Make sure the theme they choose exists
+	    setcookie("sitestyle", $choice, time()+60*60*24*100, "/"); // COOKIE SET FOR 30 DAYS
+	    header("location: template.php"); // Send the user anywhere here after setting theme
+	    exit(); // Terminate this script and page
+	}
+}
+?>
+<?php 
 include 'style/css_cookie_check.php'; // Check if the user has a style cookie set and access its value
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
